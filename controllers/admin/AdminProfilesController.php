1bc111f2136cd156b7a30a2ff63224fa8ed41c8b
Jorge Serra Sánchez <jorgeserrasanchez@gmail.com>
2013-01-18 11:03:54 +0100

    Change-Id: I0c4ffa1dec919f1cdcc1294a69ac88fd01d0757e

---
+++ controllers/admin/AdminProfilesController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * "Descripción del fichero"
+ * @package "Paquete del fichero"
+ * @subpackage "Subpaquete del fichero"
+ * @category "Cateroria del fichero"
+ * @author Jorge Serra Sanchez
+ * @copyright "Derechos de autor"
+ * @version "Version del fichero"
+ * @filesource
+ */
+
+?>
